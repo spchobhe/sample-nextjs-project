9f0a02e52677f7f4fceb8f6d2c54d34c07d02ac9
spchobhe <53969460+spchobhe@users.noreply.github.com>
2023-06-23 16:23:09 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+      reactStrictMode: true,
+
+       // Add basePath
+        basePath: '/github-pages',
       output: 'export'
 }
 
