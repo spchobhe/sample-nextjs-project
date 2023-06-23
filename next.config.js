f616b649f3f61cc1605c7106dece1f549db94395
spchobhe <spchobhe@live.com>
2023-06-23 16:59:03 -0400

    worked on the next js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,7 @@ const nextConfig = {
       reactStrictMode: true,
 
        // Add basePath
-        basePath: '/sample-nextjs-project/',
       output: 'export',
-      images: {
-    loader: "akamai",
-    path: "/sample-nextjs-project/",
-  },
 }
 
 module.exports = nextConfig
